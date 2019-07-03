3215b2c1c095f2c26e10832ba6806315a0636726
Fernando Gonçalves <fernando.goncalves@minutoseguros.com.br>
2019-07-03 11:50:24 -0300

    Função de adição de novo contato criada

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,56 +1,94 @@
 import React, { Component } from 'react'
+import { Context } from '../../context';
+import uuid from 'uuid';
 
 class AddContact extends Component {
 
-
     state = {
+        id: '',
         name: '',
         email: '',
         phone: ''
     }
 
+    onChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    onSubmit = (dispatch, e) => {
+        e.preventDefault();
+
+        const { name, email, phone } = this.state;
+
+        const newContact = {
+            id: uuid(),
+            name,
+            email,
+            phone
+        }
+
+        dispatch({type: 'ADD_CONTACT', payload: newContact});
+
+        this.setState({
+            name: '',
+            email: '',
+            phone: ''
+        })
+    } 
+
     render() {
         const { name, email, phone } = this.state;
         return (
-            <div className="card mb-3">
-                <div className="card-header">Add Contact</div>
-                <div className="card-body">
-                    <form>
-                        <div className="form-group">
-                            <label htmlFor="name">Name</label>
-                            <input 
-                                type="text" 
-                                value={name} 
-                                name="name" 
-                                className="form-control form-control-lg" 
-                                placeholder="Enter the name"
-                            />
+            <Context.Consumer>
+                {value => {
+                    const { dispatch }  = value;
+                    return(
+                        <div className="card mb-3">
+                            <div className="card-header">Add Contact</div>
+                            <div className="card-body">
+                                <form onSubmit={this.onSubmit.bind(this, dispatch)}>
+                                    <div className="form-group">
+                                        <label htmlFor="name">Name</label>
+                                        <input 
+                                            type="text" 
+                                            value={name} 
+                                            name="name" 
+                                            className="form-control form-control-lg" 
+                                            placeholder="Enter the name"
+                                            onChange={this.onChange}
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="name">Email</label>
+                                        <input 
+                                            type="text" 
+                                            value={email} 
+                                            name="email" 
+                                            className="form-control form-control-lg" 
+                                            placeholder="Enter the email"
+                                            onChange={this.onChange}
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="name">Phone</label>
+                                        <input 
+                                            type="text" 
+                                            value={phone} 
+                                            name="phone" 
+                                            className="form-control form-control-lg" 
+                                            placeholder="Enter the phone"
+                                            onChange={this.onChange}
+                                        />
+                                    </div>
+                                    <input type="submit" value="Add Contact" className="btn btn-light btn-block"/>
+                                </form>
+                            </div>                                
                         </div>
-                        <div className="form-group">
-                            <label htmlFor="name">Email</label>
-                            <input 
-                                type="text" 
-                                value={email} 
-                                name="email" 
-                                className="form-control form-control-lg" 
-                                placeholder="Enter the email"
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="name">Phone</label>
-                            <input 
-                                type="text" 
-                                value={phone} 
-                                name="phone" 
-                                className="form-control form-control-lg" 
-                                placeholder="Enter the phone"
-                            />
-                        </div>
-                        <input type="submit" value="Add Contact" className="btn btn-light btn-block"/>
-                    </form>
-                </div>
-                                
-            </div>
+                    );
+                }}                
+            </Context.Consumer>
         )
     }
 }
