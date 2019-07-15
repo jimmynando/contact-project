1b4c2140128a23df5a7a162e79796f24cbd8ac43
Fernando Gonçalves <fernando.goncalves@minutoseguros.com.br>
2019-07-15 10:32:24 -0300

    Updates

---
+++ precache-manifest.a1f0fcd261111c099dcd5a5c472323ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e8ed0c5175d8e5a5cd8a3f3d8d4fcf",
+    "url": "/contact-project/index.html"
+  },
+  {
+    "revision": "4a2fd28da6bf3375dc79",
+    "url": "/contact-project/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "5e0825f56636be35423e",
+    "url": "/contact-project/static/css/main.e9639a40.chunk.css"
+  },
+  {
+    "revision": "4a2fd28da6bf3375dc79",
+    "url": "/contact-project/static/js/2.3745af0c.chunk.js"
+  },
+  {
+    "revision": "5e0825f56636be35423e",
+    "url": "/contact-project/static/js/main.30be8b5a.chunk.js"
+  },
+  {
+    "revision": "5b2f9863f661949821e5",
+    "url": "/contact-project/static/js/runtime~main.0cd75b38.js"
+  }
+]);
